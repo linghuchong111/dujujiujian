762b5278ae1137afbffdf8336edca865c5c73e22
linghuchong111 <3372263185@qq.com>
2017-08-09 19:36:46 +0800

    新剑法

--- src/com/atguigu/jianfa/NewJian.java
+++ src/com/atguigu/jianfa/NewJian.java
@@ -3,8 +3,7 @@ package com.atguigu.jianfa;
 public class NewJian {
 
 	public static void main(String[] args) {
-		
-		System.out.println("new jianfa");
+		System.out.println("new xinjianfa");
 	}
 
 }
